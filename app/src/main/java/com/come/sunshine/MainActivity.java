b347465f162dbab96ac9587e0874fd9c59ddb37e
torrentcome <come@netcosport.com>
2015-07-07 18:50:18 +0200

    toto

--- app/src/main/java/com/come/sunshine/MainActivity.java
+++ app/src/main/java/com/come/sunshine/MainActivity.java
@@ -11,8 +11,12 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new ForecastFragment())
+					.commit();
+		}
 	}
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,3 +40,47 @@ public class MainActivity extends AppCompatActivity {
 		return super.onOptionsItemSelected(item);
 	}
 }
+
+/*
+save main activity:
+<fragment xmlns:android="http://schemas.android.com/apk/res/android"
+          xmlns:tools="http://schemas.android.com/tools"
+          android:id="@+id/fragment"
+          android:name="com.come.sunshine.ForecastFragment"
+          tools:layout="@layout/fragment_main"
+          android:layout_width="match_parent"
+          android:layout_height="match_parent" />
+
+fragment main:
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+                xmlns:tools="http://schemas.android.com/tools"
+                android:layout_width="match_parent"
+                android:layout_height="match_parent"
+                android:paddingLeft="@dimen/activity_horizontal_margin"
+                android:paddingRight="@dimen/activity_horizontal_margin"
+                android:paddingTop="@dimen/activity_vertical_margin"
+                android:paddingBottom="@dimen/activity_vertical_margin"
+                tools:context=".MainActivityFragment"
+                tools:showIn="@layout/activity_main">
+
+	<ListView
+		android:id="@+id/listview_forecast"
+		android:layout_width="wrap_content"
+		android:layout_height="wrap_content"/>
+
+
+
+</RelativeLayout>
+
+save list_item
+<?xml version="1.0" encoding="utf-8"?>
+
+<TextView xmlns:android="http://schemas.android.com/apk/res/android"
+          android:layout_width="match_parent"
+          android:layout_height="wrap_content"
+          android:minHeight="?android:attr/listPreferredItemHeight"
+          android:gravity="bottom|center"
+          android:id="@+id/list_item_forecast_textview" />
+
+ */
+
